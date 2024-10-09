941a2326184cac2a70e2d716e815e50ddae467a9
Hamza Sama <naveedhamza52@gmail.com>
2024-10-09 16:45:14 +0500

    FaQs
    javascript toggle

---
+++ FaQs/FaQs.js
@@ -0,0 +1,56 @@
+function toggle() {
+  const minusImg = document.querySelector(".minus");
+  const plusImg = document.querySelector(".plus");
+  var para = document.querySelector(".para");
+  if (para.style.display === "none") {
+    para.style.display = "block";
+    plusImg.style.display = "none";
+    minusImg.style.display = "block";
+  } else {
+    para.style.display = "none";
+    minusImg.style.display = "none";
+    plusImg.style.display = "block";
+  }
+}
+function toggle2() {
+  const minusImg2 = document.querySelector(".minus2");
+  const plusImg2 = document.querySelector(".plus2");
+  var para = document.querySelector(".para2");
+  if (para.style.display === "none") {
+    para.style.display = "block";
+    plusImg2.style.display = "none";
+    minusImg2.style.display = "block";
+  } else {
+    para.style.display = "none";
+    minusImg2.style.display = "none";
+    plusImg2.style.display = "block";
+  }
+}
+function toggle3() {
+    const minusImg3 = document.querySelector(".minus3");
+    const plusImg3 = document.querySelector(".plus3");
+  var para = document.querySelector(".para3");
+  if (para.style.display === "none") {
+    para.style.display = "block";
+    plusImg3.style.display = "none";
+    minusImg3.style.display = "block";
+  } else {
+    para.style.display = "none";
+    minusImg3.style.display = "none";
+    plusImg3.style.display = "block";
+  }
+}
+function toggle4() {
+    const minusImg4 = document.querySelector(".minus4");
+    const plusImg4 = document.querySelector(".plus4");
+  var para = document.querySelector(".para4");
+  if (para.style.display === "none") {
+    para.style.display = "block";
+    plusImg4.style.display = "none";
+    minusImg4.style.display = "block";
+  } else {
+    para.style.display = "none";
+    minusImg4.style.display = "none";
+    plusImg4.style.display = "block";
+  }
+}
